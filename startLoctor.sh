97e97b902ab291bb9c7999e4b14dc06c7c0fb56c
AkihiroKitada <akitada@pivotal.io>
2017-10-23 15:05:32 +0900

    Added scripts to start a locator and a server

---
+++ startLoctor.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+gfsh start locator --name=locator --bind-address=172.16.227.2 --port=55221 --dir=locator --max-heap=512m --properties-file=resources/gemfire.properties
---
+++ startServer.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+gfsh start server --name=server --dir=server --cache-xml-file=resources/cache.xml --properties-file=resources/gemfire.properties
